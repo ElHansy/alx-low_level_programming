747d304ac9c24bd4384158bd76ad36df07b2865d
Muhammad ElHansy <Muhammadelhansy@gmail.com>
2023-10-05 16:18:54 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
- * main - main function
+ * main - Entry point
  *
- * Return: 0 always
+ * Return: always 0 (success)
  */
-
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return(1);
 }
+
