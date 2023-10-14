4efdfeae0572631166fad1c874f089d547f5d092
Muhammad ElHansy <Muhammadelhansy@gmail.com>
2023-10-14 08:02:53 -0700

    ok

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,21 +3,17 @@
 /**
  * print_alphabet_x10 - entry point
  *
- * Return: void
 */
 
 void print_alphabet_x10(void)
 {
 	char c;
-	int i = 0;
+	int i;
 
-	while (i <= 9)
+	for (i = 0; i <= 9; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
-		{
 			_putchar(c);
-			_putchar('\n');
-		}
-		i++;
+		_putchar('\n');
 	}
 }
