8f2a329a65bd3f297a73e614f5ca222576591b8a
Muhammad ElHansy <Muhammadelhansy@gmail.com>
2023-10-15 01:10:08 -0700

    ok

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main -entry
+ * Description: fibonacci
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	int total_sum;
+
+	while (1)
+	{
+		sum = fib1 + fib2;
+
+		if (sum > 4000000)
+			break;
+		if ((sum % 2) == 0)
+			total_sum += sum;
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	printf("%.0f\n", total_sum);
+	return (0);
+}
