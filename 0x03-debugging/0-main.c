2fcb9ab9f40960c185d982f7538b4180eceecaf2
Muhammad ElHansy <Muhammadelhansy@gmail.com>
2023-10-15 11:14:31 -0700

    ok

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,22 +7,6 @@
 * Return: 0
 */
 
-void positive_or_negative(int i)
-{
-	if (i > 0)
-	{
-		printf("%i is positive\n", i);
-	}
-	else if (i == 0)
-	{
-		printf("%i is zero\n", i);
-	}
-	else
-	{
-		printf("%i is negative\n", i);
-	}
-}
-
 int main(void)
 {
 	int i;
