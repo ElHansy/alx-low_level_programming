1c94ff32d2e9bbb703429f9f882f09fbf88b92b6
Muhammad ElHansy <Muhammadelhansy@gmail.com>
2023-10-21 10:28:28 +0300

    ok

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,15 +10,18 @@
 
 void print_number(int n)
 {
+	unsigned int m;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
 	}
 
-	if (n >= 10)
-	{
-		print_number(n / 10);
-	}
-	_putchar(n % 10 + '0');
+	m = n;
+
+	if (m / 10)
+		print_number(m / 10);
+
+	_putchar(m % 10 + '0');
 }
