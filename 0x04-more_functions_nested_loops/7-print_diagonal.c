7a95c91718af8c35e69aea4d2c39cc289286d813
Muhammad ElHansy <Muhammadelhansy@gmail.com>
2023-10-23 12:56:17 +0300

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal line
+ *
+ * @n: number of \\ to be printed
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i = 0, j;
+
+	if (i == 0)
+		_putchar('\n');
+
+	while (i < n && n > 0)
+	{
+		j = 0;
+
+		while (j < i)
+		{
+			_putchar(' ');
+			j++;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+
+}
