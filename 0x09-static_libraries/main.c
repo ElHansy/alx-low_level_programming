1257cbb80a0f2a721412adeb8a18dc8bd421bd06
Muhammad ElHansy <Muhammadelhansy@gmail.com>
2023-11-01 01:04:33 +0300

    stat lib

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,14 @@
 #include "main.h"
 
+/**
+ * main - main function
+ * Description: main
+ * Return: 0 always
+*/
+
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t
+			- Kevin Mitnick");
+	return (0);
 }
