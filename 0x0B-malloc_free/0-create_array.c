286deed37ab8a42425850b4357af2e7bac4291f7
Muhammad ElHansy <Muhammadelhansy@gmail.com>
2023-11-03 17:11:09 +0300

    malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+
+#include "main.h"
+
+/**
+ * *create_array - Create array and initialize it with character c
+ * @size: of array
+ * @c: input char
+ * Return: pointer to array or null
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *P = malloc(sizeof(char) * size);
+
+	if (size == 0 || P == 0)
+		return (NULL);
+	else
+	{
+		while (size--)
+		{
+			P[size] = c;
+		}
+	}
+	return (P);
+}
