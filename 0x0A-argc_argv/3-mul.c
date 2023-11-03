dde830d13247044ca1325592b744f4543a358384
Muhammad ElHansy <Muhammadelhansy@gmail.com>
2023-11-03 11:12:25 +0300

    ok

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,16 +8,16 @@
  * Return: 0
 */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	if(argc <= 2)
+	if (argc <= 2)
 	{
 		printf("%s\n", "Error");
 		return (1);
 	}
 	else
 	{
-		printf("%i\n", (atoi(argv[argc - 1])) * (atoi(argv[argc -2])));
+		printf("%i\n", (atoi(argv[argc - 1])) * (atoi(argv[argc - 2])));
 	}
 	return (0);
 }
