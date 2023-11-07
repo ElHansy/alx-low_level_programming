e70f0f9aa9cc2609f6c1360327e636d6160ef9d8
Muhammad ElHansy <Muhammadelhansy@gmail.com>
2023-11-07 12:43:49 +0300

    ok

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+
+/**
+ * struct dog - to define dog info
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: dog info
+ */
+
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
