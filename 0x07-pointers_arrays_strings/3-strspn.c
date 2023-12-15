b23642f4b8ed12309cd75e8bd92d84d01ea2af7c
Muhammad ElHansy <Muhammadelhansy@gmail.com>
2023-12-15 16:45:23 +0300

    cal-length

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strspn - Get the length of initial matched prefix
+ * @s: string to be scanned
+ * @accept:string containing the characters to match
+ * Return:Returns the number of bytes in the initial segment
+ * of s which consist only of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int match;
+
+	for (int i = 0; s[i] != '\0'; i++)
+	{
+		match = 0;
+
+		for (int j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				match = 1;
+				break;
+			}
+		}
+
+		if (!match)
+		{
+			return (count);
+		}
+		count++;
+	}
+	return (count);
+}
