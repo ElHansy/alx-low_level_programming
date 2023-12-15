802bff244126bd9f24ccc11c90f6f34fe1e2d799
Muhammad ElHansy <Muhammadelhansy@gmail.com>
2023-12-15 16:53:44 +0300

    modify

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,12 +12,13 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int count = 0;
 	int match;
+	int i,j;
 
-	for (int i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		match = 0;
 
-		for (int j = 0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
