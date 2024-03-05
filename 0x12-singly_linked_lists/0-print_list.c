14aaeb8cbf0c120aa03981680ac9790ece4f1a50
Muhammad ElHansy <Muhammadelhansy@gmail.com>
2024-03-05 22:52:23 +0300

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,38 +1,24 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * STRlen - return length of string
- * @s: the string whose length to check
- * Return: integer length of string
- */
-
-int STRlen(const char *s)
-{
-	int a = 0;
-
-	if (!s)
-		return (0);
-	while (*s++)
-		a++;
-	return (a);
-}
-
-/**
- * print_list - prints a linked lists
- * @h: pointer to first node
- * Return: size of list
+ * print_list - prints list_t list
+ * @h: pointer to head list
+ * Return: the number of nodes, and [0] if string is NULL
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t b = 0;
+	size_t i = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		printf("[%d] %s\n", STRlen(h->str), h->str ? h->str : "(nil)");
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		i++;
 		h = h->next;
-		b++;
 	}
-	return (b);
+	return (i);
 }
