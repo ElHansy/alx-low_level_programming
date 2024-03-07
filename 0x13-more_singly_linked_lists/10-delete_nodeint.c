0821c7ddef579612edbc741ca12a4911e499d56a
Muhammad ElHansy <Muhammadelhansy@gmail.com>
2024-03-07 23:20:20 +0300

    ok

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	node = *head;
 	while (node)
 	{
-		if (i == index)
+		if (j == index)
 		{
 			p_node->next = node->next;
 			free(node);
