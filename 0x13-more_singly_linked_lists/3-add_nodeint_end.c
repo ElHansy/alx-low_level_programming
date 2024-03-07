72e523b4e1bcfc94f5bf1e62c89d5b7704c7a3f9
Muhammad ElHansy <Muhammadelhansy@gmail.com>
2024-03-07 22:06:04 +0300

    ok

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -6,6 +6,7 @@
  * @n: new node value
  * Return: new node pointer
  */
+
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *n_node = malloc(sizeof(listint_t));
@@ -14,8 +15,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (!head || !n_node)
 		return (NULL);
 
-	new_node->next = NULL;
-	new_node->n = n;
+	n_node->next = NULL;
+	n_node->n = n;
 
 	if (!*head)
 		*head = n_node;
